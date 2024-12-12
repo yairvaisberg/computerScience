2732e579b98855f1066fbae9a009dcd87c74f8d3
yair vaisberg <yairvaisberg@gmail.com>
2024-12-12 17:40:55 +0200

    init print and get numbers and base

--- ex1/src/main/java/BaseCalculatorMain.java
+++ ex1/src/main/java/BaseCalculatorMain.java
@@ -5,15 +5,24 @@ public class BaseCalculatorMain {
 
         Scanner in = new Scanner(System.in);
 
-        BaseCaLculator base = new BaseCaLculator();
+        BaseCaLculator base = new BaseCaLculator();  //create new object base from BaseCaLculator
 
 
+        //enter the first number
+        System.out.println("Enter a string as number#1");
+        String num1 = in.nextLine();
 
+        //enter second number
+        System.out.println("Enter a string as number#2");
+        String num2 = in.nextLine();
 
+        //enter the base
+        System.out.println("enter a base to get the answer in:");
+        String Stringbase = in.nextLine();
 
 
         //run the function
-        System.out.println(base.calculate("", "",""));
+        System.out.println(base.ClalculateBiggestValue(num1, num2,Stringbase));
 
 
 
