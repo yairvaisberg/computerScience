4b799455b66818fc7c5bbeadddbcfdc630d3c5a0
yair vaisberg <yairvaisberg@gmail.com>
2024-12-12 17:15:46 +0200

    init hasb

--- ex1/src/main/java/BaseCaLculator.java
+++ ex1/src/main/java/BaseCaLculator.java
@@ -1,7 +1,28 @@
 import java.util.Scanner;
 
 public class BaseCaLculator {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+
+    public String calculate(String val1, String val2, String val3){
+        return "1";
     }
+
+
+
+    //check if the input has b to indicate base that's not 10 if it has return true
+    public  boolean hasb(String input){
+        boolean isbExist=false; //create boolean to check if b exists in String
+
+        for (int i = 0; i < input.length(); i++) {    //loop on all char in string looking for char 'b'
+            if (input.charAt(i)=='b'){
+                isbExist=true;  //b exists
+                break;  //stop looking for 'b'
+            }
+        }
+        if (!isbExist){     //if 'b' is not in string
+            return false;
+        }
+        return true; //it's not empty or null and if it contains the char 'b' in the string then return true
+    }
+
+
 }
