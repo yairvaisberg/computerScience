3f74ee444682bada63f460724c3dc8315351423c
yair vaisberg <yairvaisberg@gmail.com>
2024-12-12 17:22:08 +0200

    init scanner

---
+++ ex1/src/main/java/BaseCalculatorMain.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class BaseCalculatorMain {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        BaseCaLculator base = new BaseCaLculator();
+
+
+
+
+
+
+        //run the function
+        System.out.println(base.calculate("", "",""));
+
+
+
+
+    }
+}
